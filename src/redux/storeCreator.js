c0b60d82e683b3df67ed460cebbbd3668eb9f985
Shah <aamirs@amazon.com>
2016-06-03 17:19:08 -0700

    Remove src dependency on server config

--- src/redux/storeCreator.js
+++ src/redux/storeCreator.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 import DevTools from './DevTools'
-import config from '../../server/config'
 
 const loggerMiddleware = createLogger()
 
@@ -16,14 +15,5 @@ const enhancer = compose(
 export default function configureStore(rootReducer, reducerPath) {
   // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
   // See https://github.com/rackt/redux/releases/tag/v3.1.0
-  const store = createStore(rootReducer, enhancer); // initialState would be 2nd param
-
-  // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
-  if (config.env === 'development' && module.hot) {
-    module.hot.accept(reducerPath, () =>
-      store.replaceReducer(require(reducerPath)/*.default if you use Babel 6+ */)
-    );
-  }
-
-  return store;
+  return createStore(rootReducer, enhancer); // initialState would be 2nd param
 }
