7c058425a8f768086ef119b6a374d569d016db2e
Shah <aamirs@amazon.com>
2016-06-03 18:58:20 -0700

    Enable store middleware

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,7 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+
+const loggerMiddleware = createLogger()
+
+const enhancer = compose(
+  // Middleware you want to use in development:
+  applyMiddleware(thunkMiddleware, loggerMiddleware),
+  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+);
 
 export default function configureStore(rootReducer, initialState) { 
-  return createStore(rootReducer, 
-            initialState, 
-            typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f);
+  return createStore(rootReducer, initialState, enhancer);
 };
