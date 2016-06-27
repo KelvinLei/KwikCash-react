1b370486619a3c82b2b89ecb9e2e0c8fd07b6684
Shah <aamirs@amazon.com>
2016-06-26 20:25:21 -0700

    remove some debug logs

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,9 +3,6 @@ import { combineReducers } from 'redux'
 import memberReducers from './member/memberRootReducer.js'
 import loginReducers from './login'
 
-console.log(loginReducers)
-console.log(memberReducers)
-
 export default combineReducers(Object.assign({},
   memberReducers,
   loginReducers
