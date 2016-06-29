5ea85159b4f356f6f9dba4702b2e63c2ae56151b
Shah <aamirs@amazon.com>
2016-06-29 00:39:56 -0700

    fix broken root reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux'
 import memberReducers from './member/memberRootReducer'
 import loginReducers from './login'
 
-export default combineReducers(
+export default combineReducers(Object.assign({},
   memberReducers,
   loginReducers
-)
+))
