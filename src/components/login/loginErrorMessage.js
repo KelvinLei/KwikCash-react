adb60d14efffd42e170ccff7f8430ed61590d366
Shah <aamirs@amazon.com>
2016-07-03 16:43:18 -0700

    small refactor - replace with export default in login error component

--- src/components/login/loginErrorMessage.js
+++ src/components/login/loginErrorMessage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ErrorMessage = () => (
+export default () => (
   <ul class="parsley-errors-list filled" id="kc-incorrect-cred">
     <li class="parsley-type">
       Your email or password was incorrect. Please try again.
@@ -8,4 +8,3 @@ const ErrorMessage = () => (
   </ul>
 );
 
-export default ErrorMessage
