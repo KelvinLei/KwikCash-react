fd9d4efb17d1b4254b227202a8345d0b3d0799f0
Shah <aamirs@amazon.com>
2016-08-31 09:35:39 -0700

    Make authenticate use unauthenticated post

--- src/api/index.js
+++ src/api/index.js
@@ -28,8 +28,31 @@ const authenticatedPost = (url, input) => {
   })
 }
 
+// todo: share code with above authenticated post - had to do this in a rush due to production issue
+const unauthenticatedPost = (url, input) => {
+  var token = localStorage.getItem('user_token')
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(input)
+  }).then((response) => {
+    if (response.status == 200) {
+      return response;
+    } else {
+      var error = new Error(response.statusText)
+      error.response = response
+      throw error
+    }
+  }).then((response) => {
+    return response.json()
+  })
+}
+
 export const authenticate = (username, password) => {
-  return authenticatedPost('/api/authenticate', {
+  return unauthenticatedPost('/api/authenticate', {
     username,
     password,
   })
