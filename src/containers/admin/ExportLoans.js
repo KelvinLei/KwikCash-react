debe7d9f27b30bd392828f81adbec328a9a505aa
Lei <kelvilei@amazon.com>
2016-09-09 17:37:53 -0700

    added exportloans

---
+++ src/containers/admin/ExportLoans.js
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+export default class ExportLoans extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    )
+  }
+}
+
+// ExportLoans.propTypes = {
+// }
+//
+//
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(ExportLoans)
