4d0d546621478c983ef76f2b16944c3276e0afb0
Lei <kelvilei@amazon.com>
2016-09-17 18:39:27 -0700

    added inputs to filtering widget. integrated with pikaday for date picker

---
+++ src/components/admin/exportLoans/Calendar.js
@@ -0,0 +1,73 @@
+import Pikaday from 'pikaday/pikaday.js'
+
+export default () => {
+
+
+  // DATETIMEPICKER
+  // -----------------------------------
+
+  // $('#datetimepicker1').datetimepicker();
+  //
+  // $('#datetimepicker').datetimepicker({
+  //   icons: {
+  //     time: 'fa fa-clock-o',
+  //     date: 'fa fa-calendar',
+  //     up: 'fa fa-chevron-up',
+  //     down: 'fa fa-chevron-down',
+  //     previous: 'fa fa-chevron-left',
+  //     next: 'fa fa-chevron-right',
+  //     today: 'fa fa-crosshairs',
+  //     clear: 'fa fa-trash'
+  //   }
+  // });
+
+  // var d = $('#datepicker').pikaday({
+  //   firstDay: 1,
+  //   minDate: new Date(2000, 0, 1),
+  //   maxDate: new Date(2020, 12, 31),
+  //   yearRange: [2000,2020]
+  // });
+  // chain a few methods for the first datepicker, jQuery style!
+  // d.pikaday('show').pikaday('nextMonth');
+
+  var startDate,
+    endDate,
+    updateStartDate = function() {
+      startPicker.setStartRange(startDate);
+      endPicker.setStartRange(startDate);
+      endPicker.setMinDate(startDate);
+    },
+    updateEndDate = function() {
+      startPicker.setEndRange(endDate);
+      startPicker.setMaxDate(endDate);
+      endPicker.setEndRange(endDate);
+    },
+    startPicker = new Pikaday({
+      field: document.getElementById('start'),
+      maxDate: new Date(),
+      onSelect: function() {
+        startDate = this.getDate();
+        updateStartDate();
+      }
+    }),
+    endPicker = new Pikaday({
+      field: document.getElementById('end'),
+      maxDate: new Date(),
+      onSelect: function() {
+        endDate = this.getDate();
+        updateEndDate();
+      }
+    }),
+    _startDate = startPicker.getDate(),
+    _endDate = endPicker.getDate();
+
+  if (_startDate) {
+    startDate = _startDate;
+    updateStartDate();
+  }
+
+  if (_endDate) {
+    endDate = _endDate;
+    updateEndDate();
+  }
+}
--- src/components/admin/exportLoans/ExportLoansContent.js
+++ src/components/admin/exportLoans/ExportLoansContent.js
@@ -4,19 +4,29 @@ import { Row, Col, Panel, Table, Grid, Button } from 'react-bootstrap';
 import { Link } from 'react-router';
 import { LoadingSpinner } from '../../../components/shared/LoadingSpinner'
 import LoansDataTable from './LoansDataTable'
+import Calender from './Calendar'
+import states from './states_info'
 import styles from './LoansDataTable.scss'
+
+require('pikaday/pikaday.js')
+require('pikaday/plugins/pikaday.jquery.js')
+require('pikaday/css/pikaday.css')
 
 export default class ExportLoansContent extends Component {
 
   componentDidMount() {
     LoansDataTable()
-  }
-
-  componentWillReceiveProps(nextProps) {
+    Calender()
   }
 
   render() {
     const {} = this.props;
+
+    const statesDropdown = states.map( (state, id) => {
+      return (
+        <option key={id}>{state.abbreviation} - {state.name}</option>
+      )
+    })
 
     return (
       <ContentWrapper>
@@ -26,8 +36,90 @@ export default class ExportLoansContent extends Component {
 
         <Row>
           <Col md={ 12 }>
-            <Panel id="" className="panel-default" header="Filter Elements">
-              <h4>Hello world</h4>
+            <Panel header="Loan Filtering Widget">
+
+              <form className="form-horizontal">
+                <fieldset>
+                  <div className="form-group">
+                    <label className="col-lg-2 control-label">Fund date range</label>
+                    <Col lg={ 5 }>
+                      <input type="text" id="start" className="form-control"/>
+                      <span className="help-block m-b-none">Fund start date</span>
+                    </Col>
+                    <Col lg={ 5 }>
+                      <input type="text" id="end" className="form-control"/>
+                      <span className="help-block m-b-none">Fund end date</span>
+                    </Col>
+                  </div>
+                </fieldset>
+
+                <fieldset>
+                  <div className="form-group">
+                    <label className="col-lg-2 control-label">Loan status</label>
+                    <Col sm={ 10 }>
+                      <label className="radio-inline c-radio">
+                        <input id="inlineradio1" type="radio" name="i-radio" value="option1" defaultChecked />
+                        <em className="fa fa-circle"></em>Active
+                      </label>
+                      <label className="radio-inline c-radio">
+                        <input id="inlineradio2" type="radio" name="i-radio" value="option2" />
+                        <em className="fa fa-circle"></em>Charged off
+                      </label>
+                      <label className="radio-inline c-radio">
+                        <input id="inlineradio3" type="radio" name="i-radio" value="option3" />
+                        <em className="fa fa-circle"></em>Late
+                      </label>
+                      <label className="radio-inline c-radio">
+                        <input id="inlineradio3" type="radio" name="i-radio" value="option3" />
+                        <em className="fa fa-circle"></em>Manual
+                      </label>
+                      <label className="radio-inline c-radio">
+                        <input id="inlineradio3" type="radio" name="i-radio" value="option3" />
+                        <em className="fa fa-circle"></em>Paid
+                      </label>
+                      <label className="radio-inline c-radio">
+                        <input id="inlineradio3" type="radio" name="i-radio" value="option3" />
+                        <em className="fa fa-circle"></em>Plan
+                      </label>
+                    </Col>
+                  </div>
+                </fieldset>
+
+                <fieldset>
+                  <div className="form-group">
+                    <label className="col-sm-2 control-label">State</label>
+                    <Col sm={ 10 }>
+                      <Row>
+                        <div className="col-lg-4">
+                          <select className="form-control">
+                            { statesDropdown }
+                          </select>
+                        </div>
+                      </Row>
+                    </Col>
+                  </div>
+                </fieldset>
+
+                <fieldset>
+                  <div className="form-group">
+                    <label className="col-sm-2 control-label">More options</label>
+                    <Col sm={ 10 }>
+                      <div className="checkbox c-checkbox needsclick">
+                        <label className="needsclick">
+                          <input standalone type="checkbox" defaultChecked value="" className="needsclick" />
+                          <em className="fa fa-check"></em>Exclude Loans Under 1001</label>
+                      </div>
+                    </Col>
+                  </div>
+                </fieldset>
+
+                <div className="form-group">
+                  <Col lgOffset={ 2 } lg={ 10 }>
+                    <Button type="submit" bsStyle="default" bsSize="small">Sign in</Button>
+                  </Col>
+                </div>
+              </form>
+
             </Panel>
           </Col>
         </Row>
