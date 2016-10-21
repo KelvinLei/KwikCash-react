3b73161e3f627c15e2198afd838124e73f848a1d
Lei <kelvilei@amazon.com>
2016-10-20 22:45:10 -0700

    changed failure widget text

--- src/components/shared/FailureWidget.js
+++ src/components/shared/FailureWidget.js
@@ -8,7 +8,7 @@ export const FailureWidget = () => (
   <Panel className="panel-default" header="Error">
     <Row>
       <Col md={ 12 }>
-        Sorry, we failed to retrieve your data. Please try again or contact us.
+        Sorry, we failed to retrieve your data. Please refresh the page and try again. Feel free to contact us.
       </Col>
     </Row>
   </Panel>
